92957a76cb168a7d8d54d8b1a9bedeade515ca53
psygate <psygate@users.noreply.github.com>
2022-07-08 03:05:34 +0200

    Added vehicle management. (Boats and minecarts for now.)

---
+++ paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/configs/BetterVehiclesConfig.java
@@ -0,0 +1,85 @@
+package com.programmerdan.minecraft.simpleadminhacks.configs;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.SimpleHackConfig;
+import org.bukkit.configuration.ConfigurationSection;
+
+/**
+ * @author psygate
+ */
+public class BetterVehiclesConfig extends SimpleHackConfig {
+
+	public enum GarbageCollectVehicleStrategy {
+		DROP_ITEMSTACK,
+		REMOVE;
+	}
+
+	private boolean returnVehiclesToInventoryOnExit = true;
+	private boolean garbageCollectVehicles = true;
+	private GarbageCollectVehicleStrategy garbageCollectVehicleStrategy;
+	private long maxVehicleAgeInSeconds = 10;
+	private boolean vehicleNotifyOnRemoval = true;
+
+	private long gcIntervalInTicks = 20 * 5;
+
+	public BetterVehiclesConfig(SimpleAdminHacks plugin, ConfigurationSection base) {
+		super(plugin, base);
+	}
+
+	@Override
+	protected void wireup(ConfigurationSection config) {
+		returnVehiclesToInventoryOnExit = config.getBoolean("return_vehicle_to_inventory_on_exit");
+		garbageCollectVehicles = config.getBoolean("garbage_collect_vehicle");
+		garbageCollectVehicleStrategy = GarbageCollectVehicleStrategy.valueOf(config.getString("garbage_collect_vehicle_strategy").toUpperCase());
+		maxVehicleAgeInSeconds = config.getLong("max_boat_age_in_seconds");
+		gcIntervalInTicks = config.getLong("gc_interval_in_ticks");
+	}
+
+	public boolean isReturnVehiclesToInventoryOnExit() {
+		return returnVehiclesToInventoryOnExit;
+	}
+
+	public void setReturnVehiclesToInventoryOnExit(boolean returnVehiclesToInventoryOnExit) {
+		this.returnVehiclesToInventoryOnExit = returnVehiclesToInventoryOnExit;
+	}
+
+	public boolean isGarbageCollectVehicles() {
+		return garbageCollectVehicles;
+	}
+
+	public void setGarbageCollectVehicles(boolean garbageCollectVehicles) {
+		this.garbageCollectVehicles = garbageCollectVehicles;
+	}
+
+	public GarbageCollectVehicleStrategy getGarbageCollectVehicleStrategy() {
+		return garbageCollectVehicleStrategy;
+	}
+
+	public void setGarbageCollectVehicleStrategy(GarbageCollectVehicleStrategy garbageCollectVehicleStrategy) {
+		this.garbageCollectVehicleStrategy = garbageCollectVehicleStrategy;
+	}
+
+	public long getMaxVehicleAgeInSeconds() {
+		return maxVehicleAgeInSeconds;
+	}
+
+	public void setMaxVehicleAgeInSeconds(long maxVehicleAgeInSeconds) {
+		this.maxVehicleAgeInSeconds = maxVehicleAgeInSeconds;
+	}
+
+	public boolean isVehicleNotifyOnRemoval() {
+		return vehicleNotifyOnRemoval;
+	}
+
+	public void setVehicleNotifyOnRemoval(boolean vehicleNotifyOnRemoval) {
+		this.vehicleNotifyOnRemoval = vehicleNotifyOnRemoval;
+	}
+
+	public long getGcIntervalInTicks() {
+		return gcIntervalInTicks;
+	}
+
+	public void setGcIntervalInTicks(long gcIntervalInTicks) {
+		this.gcIntervalInTicks = gcIntervalInTicks;
+	}
+}
