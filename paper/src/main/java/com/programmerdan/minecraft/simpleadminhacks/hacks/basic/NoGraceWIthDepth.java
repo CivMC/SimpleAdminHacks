7dd05002b4335a7eacdd4becabef6566ee5345ea
Aiounnae <seabirdseyeview@outlook.com>
2023-12-06 17:56:20 +0100

    Add NoDepthWithGrace
    Make Grace Accessible but not op.

---
+++ paper/src/main/java/com/programmerdan/minecraft/simpleadminhacks/hacks/basic/NoGraceWIthDepth.java
@@ -0,0 +1,33 @@
+package com.programmerdan.minecraft.simpleadminhacks.hacks.basic;
+
+import com.programmerdan.minecraft.simpleadminhacks.SimpleAdminHacks;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHack;
+import com.programmerdan.minecraft.simpleadminhacks.framework.BasicHackConfig;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffectType;
+
+public final class NoGraceWIthDepth extends BasicHack {
+	public NoGraceWIthDepth(final SimpleAdminHacks plugin, final BasicHackConfig config) {
+		super(plugin, config);
+	}
+
+	@EventHandler(ignoreCancelled = true)
+	public void removeGraceOnMove(final PlayerMoveEvent event) {
+		if (!event.hasExplicitlyChangedPosition()) {
+			return;
+		}
+		final Player player = event.getPlayer();
+		if (!player.hasPotionEffect(PotionEffectType.DOLPHINS_GRACE)) {
+			return;
+		}
+		final ItemStack boots = player.getInventory().getBoots();
+		if (boots == null || !boots.containsEnchantment(Enchantment.DEPTH_STRIDER)) {
+			return;
+		}
+		player.removePotionEffect(PotionEffectType.DOLPHINS_GRACE);
+	}
+}
